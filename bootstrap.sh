c43791b1aaa17859af52d11474eeabcfedd4fa2d
Chris <dev@ravngr.com>
2014-05-22 11:21:38 +1000

    Updates bootstrap script to work with renamed files

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ export DOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dirs=( .dotbackup .vim .vim/colors )
 
 # List of files to link
-files=( .bash_logout .bashrc .gitconfig .profile .tmux.conf .tmux-powerlinerc .vimrc .zshrc )
+files=( bash_logout bashrc gitconfig profile tmux.conf tmux-powerlinerc vimrc zshrc )
 
 # Create directories
 for d in "${dirs[@]}"; do
@@ -18,20 +18,23 @@ done
 
 for f in "${files[@]}"; do
     # Unlink existing
-    if [ -L ~/$f ]; then
-	unlink ~/$f
+    if [ -L ~/.$f ]; then
+	unlink ~/.$f
     fi
 
     # Backup any existing files
-    if [ -e ~/$f ] && [ ! -L ~/$f ]; then
-	cp ~/$f ~/$f.bak
+    if [ -e ~/.$f ] && [ ! -L ~/.$f ]; then
+	cp ~/.$f ~/$f.bak
     fi
 
     # Create link to config file
-    ln -sf $DOT_PATH/$f ~/$f
+    ln -sf $DOT_PATH/$f ~/.$f
 done
 
 ### Install modules
-cp -R $DOT_PATH/vim-airline/{autoload,doc,plugin,t} ~/.vim
+cp -R $DOT_PATH/vim-colorschemes/colors ~/.vim/colors
+
+cp -R $DOT_PATH/Cpp11-Syntax-Support/syntax ~/.vim
 cp -R $DOT_PATH/nerdtree/{autoload,doc,lib,nerdtree_plugin,plugin,syntax} ~/.vim
+cp -R $DOT_PATH/vim-airline/{autoload,doc,plugin,t} ~/.vim
 
